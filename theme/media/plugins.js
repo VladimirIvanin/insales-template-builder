0e059f4cc72fcac4fe648b32208621d470f5a4ad
brainmurder <ivaninvv@mail.ru>
2017-01-31 17:12:23 +0300

    article content

--- theme/media/plugins.js
+++ theme/media/plugins.js
@@ -1,8 +1,8 @@
 #= require plugin.collapse
-#= require alertify.min
-#= require jquery.magnific-popup.min
 #= require plugin.validateForm
+#= require jquery.magnific-popup.min
+#= require alertify.min
 #= require ion.rangeSlider.min
 #= require plugin.ionRange.adapter
-#= require swiper.min
 #= require plugin.tabs
+#= require swiper.min
