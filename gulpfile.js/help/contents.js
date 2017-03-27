c0185793c365ad0d70c8f793a6b1822a79353861
Vladimir Ivanin <ivaninvv@mail.ru>
2017-03-27 14:56:00 +0300

    Update contents.js

--- gulpfile.js/help/contents.js
+++ gulpfile.js/help/contents.js
@@ -1,13 +1,14 @@
 var _ = require('lodash');
+var path = require('path');
 
 var Contents = function () {};
 
 Contents.prototype.getScriptFile = function (scriptsPaths) {
   let _import = '#= require ';
   let content = '';
-  _.forEach(scriptsPaths, function (style) {
-    if (!style.indexOf('/') == -1 && !style.indexOf('*') == -1) {
-      content += _import + style + '\n'
+  _.forEach(scriptsPaths, function (_script) {
+    if (_script.indexOf(path.sep) == -1) {
+      content += _import + _script + '\n'
     }
   })
   return content;
@@ -17,7 +18,7 @@ Contents.prototype.getStylesFile = function (stylesPaths) {
   let _import = '@import "';
   let content = '';
   _.forEach(stylesPaths, function (style) {
-    if (style.indexOf('/') == -1 && style.indexOf('*') == -1) {
+    if (style.indexOf(path.sep) == -1) {
       content += _import + style + '";\n'
     }
   })
