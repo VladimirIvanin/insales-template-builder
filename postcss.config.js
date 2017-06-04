a60fe11d25157394a73cd2b4948977d50bd1a924
Vladimir Ivanin <ivaninvv@mail.ru>
2017-06-05 01:20:36 +0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,14 @@
 var syntax = require('postcss-scss');
 var autoprefixer = require('autoprefixer');
+var stylefmt = require('stylefmt');
 
 module.exports = {
   plugins: [
     autoprefixer({
       browsers: ['last 20 versions'],
       cascade: false
-    })
+    }),
+    stylefmt()
   ],
   parser: syntax,
   map: false,
