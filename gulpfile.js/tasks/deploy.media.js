8ea1fe36b610e8ec2623b78a9eecaf04024368f8
Vladimir Ivanin <ivaninvv@mail.ru>
2017-07-07 15:57:50 +0300

    Update deploy.media.js

--- gulpfile.js/tasks/deploy.media.js
+++ gulpfile.js/tasks/deploy.media.js
@@ -5,6 +5,7 @@ var writeFile = require('write');
 var rename = require("gulp-rename");
 var paths = require('../config/paths.json');
 var Promise = require('promise');
+var image = require('gulp-image');
 
 gulp.task('deploy:media', function (cb) {
   return new Promise(function (resolve, reject) {
@@ -12,6 +13,7 @@ gulp.task('deploy:media', function (cb) {
       .pipe(rename(function (_path) {
         _path.dirname = "";
       }))
+      .pipe(image())
       .pipe(gulp.dest(paths.theme.media))
       .on('end', function() {
         resolve();
